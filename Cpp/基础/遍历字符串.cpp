07c939bc511b45b5fe7a81c9dfb2a75fc3e384a5
JacksonJa <2937703346@qq.com>
2024-07-12 13:51:39 +0800

    By JacksonJa

--- Cpp/基础/遍历字符串.cpp
+++ Cpp/基础/遍历字符串.cpp
@@ -6,10 +6,20 @@ using std::cout;
 using std::endl;
 using std::string;
 
+void test(const string& string1){
+    if (string1.empty()) {
+        cout << "空字符串！" << endl;
+    }
+    else if (!string1.empty()){
+        cout << "字符串长度为：" << string1.length() << endl;
+    }
+}
+
 int main(){
-    string str = "Hello, I'm Lifu!";
+    string str = "Hello, I'm Lifu!\n";
     for(char c : str){
         cout << c;
     }
+    test(str);
     return 0;
 }
