492fb3bbec5dfa96cae0ef1c343c7f85f56b3ab2
JacksonJa <2937703346@qq.com>
2024-07-12 13:34:30 +0800

    By JacksonJa

---
+++ Cpp/基础/遍历字符串.cpp
@@ -0,0 +1,15 @@
+//
+// Created by jackson on 2024/7/12.
+//
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(){
+    string str = "Hello, I'm Lifu!";
+    for(char c : str){
+        cout << c;
+    }
+    return 0;
+}
