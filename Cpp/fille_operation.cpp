d9b1d9953b8f42cb1cb52db0e7ed04d40222d259
JacksonJa <2937703346@qq.com>
2024-07-27 13:27:26 +0800

    By JacksonJa

---
+++ Cpp/fille_operation.cpp
@@ -0,0 +1,23 @@
+//
+// Created by jackson on 2024/7/27.
+//
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    string str = "hello world";
+    ofstream ofs("test");
+    if (ofs.is_open()) {
+        // 使用 << 运算符将字符串写入文件
+        ofs << str;
+
+        // 关闭文件
+        ofs.close();
+
+        cout << "字符串已成功写入文件。\n" << endl;
+    } else {
+        cerr << "无法打开文件。\n" << endl;
+    }
+    return 0;
+}
