d07b3e1a3bfc16e4cb85c99154ea6edc806b471b
JacksonJa <2937703346@qq.com>
2024-07-27 14:10:14 +0800

    By JacksonJa

--- Cpp/file_operation.cpp
+++ Cpp/file_operation.cpp
@@ -6,18 +6,6 @@
 using namespace std;
 
 int main(){
-    string str = "hello world";
-    ofstream ofs("test.txt");
-    if (ofs.is_open()) {
-        // 使用 << 运算符将字符串写入文件
-        ofs << str;
 
-        // 关闭文件
-        ofs.close();
-
-        cout << "字符串已成功写入文件。\n" << endl;
-    } else {
-        cerr << "无法打开文件。\n" << endl;
-    }
     return 0;
 }
